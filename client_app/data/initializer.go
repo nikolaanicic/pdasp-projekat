4a7e78295d006004fe45df424dc2a8f9dac96511
Nikola Anicic <nikolaanicic99@gmail.com>
2025-07-25 09:09:30 +0200

    fixed a bug in ccp-generate.sh

--- client_app/data/initializer.go
+++ client_app/data/initializer.go
@@ -1,18 +1,19 @@
 package data
 
 import (
+	channelinterface "clientapp/channel_interface"
 	"clientapp/models"
 )
 
 func GetInitialUsers() map[string]models.UserInfo {
 
 	return map[string]models.UserInfo{
-		"jj1": {UserID: "jj1", Organization: "org1", Role: models.USER},
-		"it1": {UserID: "it1", Organization: "org2", Role: models.USER},
-		"ou1": {UserID: "ou1", Organization: "org3", Role: models.USER},
-		"s1":  {UserID: "s1", Organization: "org1", Role: models.ADMIN},
-		"s2":  {UserID: "s2", Organization: "org2", Role: models.ADMIN},
-		"s3":  {UserID: "s3", Organization: "org3", Role: models.ADMIN},
+		"jj1": {UserID: "jj1", Organization: "org1", Role: models.USER, ChannelInterfaces: make(map[string]*channelinterface.ChannelInterace, 0)},
+		"it1": {UserID: "it1", Organization: "org2", Role: models.USER, ChannelInterfaces: make(map[string]*channelinterface.ChannelInterace, 0)},
+		"ou1": {UserID: "ou1", Organization: "org3", Role: models.USER, ChannelInterfaces: make(map[string]*channelinterface.ChannelInterace, 0)},
+		"s1":  {UserID: "s1", Organization: "org1", Role: models.ADMIN, ChannelInterfaces: make(map[string]*channelinterface.ChannelInterace, 0)},
+		"s2":  {UserID: "s2", Organization: "org2", Role: models.ADMIN, ChannelInterfaces: make(map[string]*channelinterface.ChannelInterace, 0)},
+		"s3":  {UserID: "s3", Organization: "org3", Role: models.ADMIN, ChannelInterfaces: make(map[string]*channelinterface.ChannelInterace, 0)},
 	}
 
 }
